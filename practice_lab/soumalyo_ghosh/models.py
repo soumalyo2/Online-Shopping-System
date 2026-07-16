8f60fb0f099e861e9728252bf3a6b81e9e19e2c5
Soumalyo Ghosh <ghoshsoumalyo2@gmail.com>
2026-07-16 08:39:11 +0530

    feat:added class in forms and updated models

--- practice_lab/soumalyo_ghosh/models.py
+++ practice_lab/soumalyo_ghosh/models.py
@@ -27,16 +27,17 @@ class Product(db.Model):
         return f"Product('{self.title}', '{self.date_posted}', '{self.category}')"   
    
 class Seller(db.Model, UserMixin):
-    id = db.column(db.Integer, primary_key=True)
-    seller_name = db.column(db.string(30), unique=True, nullable=False)
-    shop_name = db.column(db.string(30), unique=True, nullable=False)
-    seller_email = db.column(db.string(120), unique=True, nullable=False)
-    seller_password = db.column(db.string(60), nullable=False)
-    aadhar_number  = db.column(db.Integer, unique=True, nullable=False)
-    gst_number = db.column(db.Integer, unique=True, nullable=False)
-    contact_number = db.column(db.Integer, unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    seller_name = db.Column(db.String(30), unique=True, nullable=False)
+    shop_name = db.Column(db.String(30), unique=True, nullable=False)
+    seller_email = db.Column(db.String(120), unique=True, nullable=False)
+    seller_password = db.Column(db.String(60), nullable=False)
+    aadhar_number = db.Column(db.String(12), unique=True, nullable=False)
+    gst_number = db.Column(db.String(15), unique=True, nullable=False)
+    contact_number = db.Column(db.String(15), unique=True, nullable=False)
     seller_image = db.Column(db.String(20), nullable=False, default='default.jpg')
-    address = db.column(db.string(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
         return f"Seller('{self.seller_name}', '{self.shop_name}', '{self.seller_email}','{self.seller_image}')"
+    
